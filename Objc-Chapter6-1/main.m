d65a05e1792e8bf478fc6334715bbd71ceb826e6
Leif Petersen <leifthelucky@gmail.com>
2013-03-01 22:51:14 -0800

    And it begins..

--- Objc-Chapter6-1/main.m
+++ Objc-Chapter6-1/main.m
@@ -14,6 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
+        // Doooooooo It!
         NSLog(@"Hello, World!");
         
     }
