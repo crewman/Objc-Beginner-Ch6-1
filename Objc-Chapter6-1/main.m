356b607a3c8a8adc4a6a78adb763aaf90be2b2ed
Leif Petersen <leifthelucky@gmail.com>
2013-03-01 22:48:11 -0800

    Initial Commit

---
+++ Objc-Chapter6-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Objc-Chapter6-1
+//
+//  Created by Leif Petersen on 13-03-01.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
